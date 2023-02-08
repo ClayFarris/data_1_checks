b055f7c5f15d8d9798e0c1f6d5b4015587ada4f6
Clay Farris <clayfarris@gmail.com>
2023-02-07 19:37:25 -0500

    Second commit

--- data_1_check.py
+++ data_1_check.py
@@ -3,3 +3,28 @@ print('Hello World')
 Four_Arya_Satyas = ['dukkha', 'samudaya', 'nirodha', 'magga']
 print(Four_Arya_Satyas[0])
 
+shrimp = [
+    {
+    'name' : 'Mantis Shrimp',
+    'order' : 'Stomatopoda',
+    'habitat' : 'tropical, subtropical and sub-Antartic waters',
+    'length' : (3.9, 15),
+    'carnivorous?' : True},
+    {
+    'name' : 'Pistol Shrimp',
+    'order' : 'Alpheidae',
+    'habitat' : 'tropical and temperate coastal and marine waters, cold seas and freshwater caves',
+    'length' : (1.2, 2),
+    'deadliest animal?' : True
+    }
+]
+
+print(shrimp[0])
+
+print(type(shrimp[0]))
+
+print(shrimp[0]['length'])
+
+integers = (299, 792, 458, 5, 0, 0)
+
+print(integers[5])
